40459a06107e7201b56ef377e5b1604e1441db72
Adeel Ahmed <adeelahmed1995@outlook.com>
2023-08-18 08:28:52 +0100

    Add pages

---
+++ client/MeetupClone/src/App.jsx
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Events from "./pages/Events";
+import EventsCategory from "./pages/EventsCategory";
+import CreateEvent from "./pages/CreateEvent";
+import EventDetails from "./pages/EventDetails";
+import './App.css'
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/dashboard' element={<Dashboard/>}/>
+                <Route path='/events/all' element={<Events/>}/>
+                <Route path='/events/:category' element={<EventsCategory/>}/>
+                <Route path='/create/event' element={<CreateEvent/>}/>
+                <Route path='/event/:slug' element={<EventDetails/>}/>
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
